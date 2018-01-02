3067f5fcafac0e82f1981f39043f92f32c41727a
mac <mac@macs-MacBook-Pro.local>
2018-01-02 15:12:50 +0800

    Initial Commit

---
+++ tableSecond/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tableSecond
+//
+//  Created by mac on 2018/1/2.
+//  Copyright © 2018年 snow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
